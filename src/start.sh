9cc55326637fb18a07a1e86d30fbb527a9c333dc
curcuz <czkzcz.cmc@gmail.com>
2021-07-22 15:28:42 +0200

    es6 rework, supervisor v2 endpoint Change-type: major Signed-off-by: Carlo Maria Curinga (@curcuz) <carlo@balena.io>

--- src/start.sh
+++ src/start.sh
@@ -1,43 +1,15 @@
 #!/usr/bin/env bash
 
-export DEFAULT_VERSION=2.0.1
-FIRMWARE_PATH=/data/firmware/firmata-
-RELEASE_PATH=https://github.com/balena-io/balena-fin-coprocessor-firmata/releases/download/
-
 if ! test -f /data/firmware/bootloader.s37 ;
     then
         echo "Copying bootloader.s37"
         cp openocd/bootloader.s37 /data/firmware/bootloader.s37
 fi
 
-if [[ "$FIRMATA_VERSION" == "latest" ]];
+if ! test -f /data/firmware/firmata-v2.0.1.hex  ;
     then
-        DEFAULT_VERSION=$(curl --silent "https://api.github.com/repos/balena-io/balena-fin-coprocessor-firmata/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-elif [[ ! -z $FIRMATA_VERSION ]]
-    then
-        DEFAULT_VERSION="${FIRMATA_VERSION}"
+        echo "Copying firmata-v2.0.1.hex"
+        cp openocd/firmata-v2.0.1.hex /data/firmware/firmata-v2.0.1.hex
 fi
-
-v=${DEFAULT_VERSION:0:1}
-if [ "$v" != "v" ];
-    then
-        DEFAULT_VERSION="v${DEFAULT_VERSION}"
-fi
-
-FILEPATH="${FIRMWARE_PATH}${DEFAULT_VERSION}.hex"
-
-echo "Firmata version set to ${DEFAULT_VERSION}"
-
-if test -f $FILEPATH
-    then
-        echo "Firmware already downloaded. Skipping."
-    else
-        echo "Downloading ${DEFAULT_VERSION} firmata firmware."
-        FULL_PATH="${RELEASE_PATH}${DEFAULT_VERSION}/firmata.hex"
-        echo "${FULL_PATH}"
-        curl ${FULL_PATH} -L -o "${FILEPATH}"
-fi
-
-export SELECTED_VERSION=$DEFAULT_VERSION
 
 node index.js

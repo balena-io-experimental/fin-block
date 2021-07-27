ed7daaea06747391c169aa9090716b46e612a903
curcuz <czkzcz.cmc@gmail.com>
2021-07-27 13:47:23 +0200

    add back more refactored features, make flasher and firmata modules singletons
    Change-type: minor
    Signed-off-by: Carlo Maria Curinga <carlo@balena.io>

---
+++ src/utils/eeprom/index.js
@@ -0,0 +1,17 @@
+#!/bin/env node
+
+const gi = require('node-gtk');
+const sleep = require('sleep-promise');
+Fin = gi.require('Fin', '0.2')
+const fin = new Fin.Client()
+
+module.exports = class eeprom {
+
+    async info() {
+        let data = {};
+        data.revision = await fin.revision;
+        data.uid = await fin.uid;
+        data.eeprom = await fin.eeprom;
+        return data;
+    }
+}
